fe89dbce46e0b8ef30498d259b84aaba27cf1b6b
Clement Lin <57208162+clement55688@users.noreply.github.com>
2026-09-15 01:45:31 -0700

    Create BookCard.jsx

---
+++ src/BookCard.jsx
@@ -0,0 +1,8 @@
+export default function BookCard({ book }) {
+  // TODO: title (h4), author, optional year inside article.book.
+  return (
+    <article className="book">
+      <h4>Book title</h4>
+    </article>
+  );
+}
